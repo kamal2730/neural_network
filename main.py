c46b0bceb95d989e3a0770948679ae64bb5ed728
kamal2730 <itzkamalkant@gmail.com>
2025-05-25 15:35:48 +0530

    added regularization techniques

--- main.py
+++ main.py
@@ -6,15 +6,32 @@ from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
 
 class Layer_Dense:
-    def __init__(self,n_inputs,n_neurons):
+    def __init__(self,n_inputs,n_neurons,weight_regularizer_l1=0,weight_regularizer_l2=0,bias_regularizer_l1=0,bias_regularizer_l2=0):
         self.weights=0.01*np.random.randn(n_inputs,n_neurons)
         self.biases=np.zeros((1,n_neurons))
+        self.weight_regularizer_l1=weight_regularizer_l1
+        self.weight_regularizer_l2=weight_regularizer_l2
+        self.bias_regularizer_l1=bias_regularizer_l1
+        self.bias_regularizer_l2=bias_regularizer_l2
     def forward(self,inputs):
         self.inputs=inputs
         self.output=np.dot(inputs,self.weights)+self.biases
     def backward(self,dvalues):
         self.dweights=np.dot(self.inputs.T,dvalues)
         self.dbiases=np.sum(dvalues,axis=0,keepdims=True)
+        #regularization
+        if self.weight_regularizer_l1>0:
+            dL1=np.ones_like(self.weights)
+            dL1[self.weights<1]=-1
+            self.dweights+=self.weight_regularizer_l1*dL1
+        if self.weight_regularizer_l2>0:
+            self.dweights+=2*self.weight_regularizer_l2*self.weights
+        if self.bias_regularizer_l1>0:
+            dL1=np.ones_like(self.biases)
+            dL1[self.biases<1]=-1
+            self.dbiases+=self.bias_regularizer_l1*dL1
+        if self.bias_regularizer_l2>0:
+            self.dbiases+=2*self.bias_regularizer_l2*self.biases
         self.dinputs=np.dot(dvalues,self.weights.T)
 class Activation_ReLU:
     def forward(self,inputs):
@@ -37,6 +54,18 @@ class Activation_Softmax:
             self.dinputs[index]=np.dot(jacobian_matrix,single_dvalues)
 
 class Loss:
+    def regularization_loss(self,layer):
+        regularization_loss=0
+        if layer.weight_regularizer_l1>0:
+            regularization_loss+=layer.weight_regularizer_l1*np.sum(np.abs(layer.weights))
+        if layer.weight_regularizer_l2>0:
+            regularization_loss+=layer.weight_regularizer_l2*np.sum(layer.weights*layer.weights)
+        if layer.bias_regularizer_l1>0:
+            regularization_loss+=layer.bias_regularizer_l1*np.sum(np.abs(layer.biases))
+        if layer.bias_regularizer_l2>0:
+            regularization_loss+=layer.bias_regularizer_l2*np.sum(layer.biases*layer.biases)
+        return regularization_loss
+    
     def calculate(self,output,y):
         sample_losses=self.forward(output,y)
         data_loss=np.mean(sample_losses)
@@ -130,27 +159,39 @@ class Optimizer_Adam:
         layer.bias_cache=self.beta_2*layer.bias_cache+(1-self.beta_2)*layer.dbiases**2
 
         weight_cache_corrected=layer.weight_cache/(1-self.beta_2**(self.iterations+1))
-        bias_cache_corrected=layer.bias_cache/(1-self.beta_2**(self.iterations+1))
+        bias_cache_corrected=layer.bias_cache/(1-self.beta_2**(self.iterations+1) )
 
         layer.weights+=-self.current_learning_rate*weight_momentums_corrected/(np.sqrt(weight_cache_corrected)+self.epsilon)
         layer.biases+=-self.current_learning_rate*bias_momentums_corrected/(np.sqrt(bias_cache_corrected)+self.epsilon)
     def post_update_params(self):
         self.iterations+=1
+class Layer_Droput:
+    def __init__(self,rate):
+        self.rate=1-rate
+    def forward(self,inputs):
+        self.inputs=inputs
+        self.binary_mask=np.random.binomial(1,self.rate,size=inputs.shape)/self.rate
+        self.output=inputs*self.binary_mask
+    def backward(self,dvalues):
+        self.dinputs=dvalues*self.binary_mask
+X,y=spiral_data(samples=1000,classes=3)
+plt.scatter(X[:,0],X[:,1],c=y,cmap='brg')
 
-X,y=spiral_data(samples=100,classes=3)
-#plt.scatter(X[:,0],X[:,1],c=y,cmap='brg')
-#plt.show()
 
-dense1=Layer_Dense(2,64)
+dense1=Layer_Dense(2,512,weight_regularizer_l2=5e-4,bias_regularizer_l2=5e-4)
 activation1=Activation_ReLU()
-dense2=Layer_Dense(64,3)
+dropout1=Layer_Droput(0.1)
+dense2=Layer_Dense(512,3)
 loss_activation=Activation_Softmax_Loss_CategoricalCrossentropy()
-optimizer=Optimizer_Adam(learning_rate=0.05,decay=5e-7)
+optimizer=Optimizer_Adam(learning_rate=0.05,decay=5e-5)
 for epoch in range(10001):
     dense1.forward(X)
     activation1.forward(dense1.output)
-    dense2.forward(activation1.output)
-    loss=loss_activation.forward(dense2.output,y)
+    dropout1.forward(activation1.output)
+    dense2.forward(dropout1.output)
+    data_loss=loss_activation.forward(dense2.output,y)
+    regularization_loss=loss_activation.loss.regularization_loss(dense1)+loss_activation.loss.regularization_loss(dense2)
+    loss=data_loss+regularization_loss
 
     predictions=np.argmax(loss_activation.output,axis=1)
     if len(y.shape)==2:
@@ -159,11 +200,12 @@ for epoch in range(10001):
 
 
     if not epoch%100:
-        print(f'epoch:{epoch},acc:{accuracy:.3f},loss:{loss:.3f},lr:{optimizer.current_learning_rate}')
+        print(f'epoch:{epoch},acc:{accuracy:.3f},loss:{loss:.3f},data_loss:{data_loss},reg_loss:{regularization_loss},lr:{optimizer.current_learning_rate}')
     #backward pass
     loss_activation.backward(loss_activation.output,y)
     dense2.backward(loss_activation.dinputs)
-    activation1.backward(dense2.dinputs)
+    dropout1.backward(dense2.dinputs)
+    activation1.backward(dropout1.dinputs)
     dense1.backward(activation1.dinputs)
 
     optimizer.pre_update_params()
@@ -185,3 +227,6 @@ if len(y_test.shape)==2:
 accuracy=np.mean(predictions==y_test)
 
 print('validation accuracy:',accuracy)
+
+plt.scatter(X_test[:,0],X_test[:,1],c=y_test,cmap='magma')
+#plt.show()
